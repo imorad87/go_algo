05b3542db90b5832a768a46432ec5e1144a600a0
Islam Morad <imorad87@gmail.com>
2023-01-07 22:01:34 +0000

    add lib requirements

--- backtesting/strategies/sma_v1.py
+++ backtesting/strategies/sma_v1.py
@@ -3,11 +3,17 @@ import yfinance as yf
 import talib as ta
 import matplotlib.pyplot as plt
 
-price_data = yf.Ticker('spy').history(period='12mo', interval='1d')
+asset = 'SPY'  # the asset to backtest
+
+price_data = yf.Ticker(asset).history(period='12mo', interval='1d')
 price_data = price_data[['Open', 'Low', 'High', 'Close', 'Volume']]
 
 # sma = ta.SMA(price_data.Close, 20)
-sma = price_data.rolling(20).mean()
+sma = price_data.Close.rolling(20).mean()
+
+plot = bt.merge(price_data, sma).plot(figsize=(15, 5))
+
+plot
 
 strat = bt.Strategy('Simple SMA', [
     bt.algos.SelectWhere(price_data > sma),
@@ -18,6 +24,7 @@ strat = bt.Strategy('Simple SMA', [
 # Create the backtest and run it
 bt_backtest = bt.Backtest(strat, price_data)
 bt_result = bt.run(bt_backtest)
+
 # Plot the backtest result
 bt_result.plot(title='Backtest result')
 plt.show()
